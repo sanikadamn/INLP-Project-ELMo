9e7395ff322050114924c813cb3673a597b288b0
sanikadamn <sanika.damle@research.iiit.ac.in>
2024-04-09 21:43:37 +0530

    elmo for marathi trained

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,7 @@ import re
 from torchtext.vocab import build_vocab_from_iterator, Vocab
 from typing import List, Tuple, Optional
 from torch.nn.utils.rnn import pad_sequence
+torch.manual_seed(0)
 
 def tokenize(sentences):
     # remove punctuations
@@ -34,6 +35,8 @@ def split_into_characters(tokenized_data, word_length):
             # pad the word to a fixed length
             if len(word_chars) < word_length:
                 word_chars += ['<PAD>'] * (word_length - len(word_chars))
+            if len(word_chars) > word_length:
+                word_chars = word_chars[:word_length]
             sentence_chars.append(word_chars)
         sentences.append(sentence_chars)
     # add padding to characters
@@ -70,7 +73,7 @@ END_TAG = '<EOS>'
 
 # create a torch dataset
 class NextWordDataset(Dataset):
-    def __init__(self, path, start=0, end=100000, vocabulary: Optional[Vocab] = None):
+    def __init__(self, path, start=0, end=100000, vocabulary: Optional[Vocab] = None, character_vocab: Optional[Vocab] = None):
         self.sentences = []
         with open(path, "r", encoding="utf-8") as f:
             for i, line in enumerate(f):
@@ -80,14 +83,15 @@ class NextWordDataset(Dataset):
                     break
         f.close()
         self.sentences = tokenize(self.sentences)
-        print("Tokenized data")
         if vocabulary is None:
             self.vocab = build_vocab_from_iterator(self.sentences, specials=[OUT_OF_VOCAB, PAD_TAG, START_TAG, END_TAG])
             self.vocab.set_default_index(self.vocab[OUT_OF_VOCAB])
         else:
             self.vocab = vocabulary
 
-        self.vocab.set_default_index(self.vocab['<OOV>'])
+        # set default index for the vocabulary
+
+        self.character_vocab = character_vocab
         # iterate over the dataset
     def __len__(self):
         return len(self.sentences)
@@ -99,7 +103,7 @@ class NextWordDataset(Dataset):
         return torch.tensor([self.vocab[word] for word in self.sentences[idx]], dtype=torch.long)
 
     
-    def format(self, batch, window_size) -> Tuple[torch.Tensor, torch.Tensor]:
+    def format(self, batch, window_size, returnvocab = False) -> Tuple[torch.Tensor, torch.Tensor]:
         # convert the batch into a tensor
         # convert the batch into a tensor
         # pad the batch
@@ -111,17 +115,23 @@ class NextWordDataset(Dataset):
                 words.append(sentence[i:i+window_size])
                 targets.append(sentence[i+window_size])
         # convert the words and targets into tensors
-        self.words, self.character_vocab = split_into_characters(words, 10)
+        self.words, character_vocab = split_into_characters(words, 10)
+        if self.character_vocab is None:
+            self.character_vocab = character_vocab
         # convert self.vocab into a dictionary and self.character_vocab into a dictionary
-        vocab = {}
-        for i, word in enumerate(self.vocab.get_itos()):
-            vocab[word] = i
-        character_vocab = {}
-        for i, char in enumerate(self.character_vocab.keys()):
-            character_vocab[char] = i
-        words_tensor, targets = convert_to_tensors(self.words, targets, vocab, character_vocab)
-        return words_tensor, targets, vocab, character_vocab
-
+        # if we are getting the vocabulary for the first time
+        if returnvocab:
+            vocab = {}
+            for i, word in enumerate(self.vocab.get_itos()):
+                vocab[word] = i
+            character_vocab = {}
+            for i, char in enumerate(self.character_vocab.keys()):
+                character_vocab[char] = i
+            words_tensor, targets = convert_to_tensors(self.words, targets, vocab, character_vocab)
+            return words_tensor, targets, vocab, character_vocab
+        else:
+            words_tensor, targets = convert_to_tensors(self.words, targets, self.vocab, self.character_vocab)
+            return words_tensor, targets
 
 
 
